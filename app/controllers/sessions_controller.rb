f128d722b647a5aec0cf3b9c0b53e91655fe4651
Andy Edwards <andyhedwards@gmail.com>
2015-10-28 11:59:51 +1300

    Add create session to api

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(name: params[:name])
+    session[:user_id] = @user.id
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "site#index"
 
+  resources :sessions
+
   namespace :api do
     namespace :v1 do
       resources :posts, except: :destroy
