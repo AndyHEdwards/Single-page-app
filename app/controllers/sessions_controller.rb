af7121dd97f722d47eb85a3301350c96f5c75b58
Joshua Vial <joshua@enspiral.com>
2015-10-28 14:32:19 +1300

    better naming of returned json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(name: params[:name])
-    session[:user_id] = @user.id
-    render json: {user_id: @user.id, userName: @user.name}
+    user = User.find_by(name: params[:name])
+    session[:user_id] = user.id
+    render json: {user_id: user.id, userName: user.name}
+    #render json: {id: user.id, name: user.name}
   end
 end
